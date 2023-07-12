24cc34af634de854fb89e66f6813122869006041
Andresprogram12 <ibailandxd@gmail.com>
2023-07-11 22:23:23 -0600

    agrege nuevas funcionalidades

--- list-edit-router.js
+++ list-edit-router.js
@@ -8,6 +8,18 @@ let tasks = [
   { id: 3, description: 'Tarea 3', completed: true },
 ];
 
+// Obtener tareas completas
+router.get('/completed', (req, res) => {
+    const completedTasks = tasks.filter((task) => task.completed);
+    res.json(completedTasks);
+  });
+  
+  // Obtener tareas incompletas
+  router.get('/incomplete', (req, res) => {
+    const incompleteTasks = tasks.filter((task) => !task.completed);
+    res.json(incompleteTasks);
+  });
+  
 // Crear una tarea
 router.post('/create', (req, res) => {
   const newTask = req.body;
--- list-view-router.js
+++ list-view-router.js
@@ -20,4 +20,33 @@ router.get('/incomplete', (req, res) => {
   res.json(incompleteTasks);
 });
 
+
+// Crear una tarea
+router.post('/create', (req, res) => {
+    const newTask = req.body;
+    tasks.push(newTask);
+    res.json({ message: 'Tarea creada correctamente' });
+  });
+  
+  // Eliminar una tarea
+  router.delete('/delete/:id', (req, res) => {
+    const taskId = req.params.id;
+    tasks = tasks.filter((task) => task.id != taskId);
+    res.json({ message: 'Tarea eliminada correctamente' });
+  });
+  
+  // Actualizar una tarea
+  router.put('/update/:id', (req, res) => {
+    const taskId = req.params.id;
+    const updatedTask = req.body;
+    tasks = tasks.map((task) => {
+      if (task.id == taskId) {
+        return { ...task, ...updatedTask };
+      }
+      return task;
+    });
+    res.json({ message: 'Tarea actualizada correctamente' });
+  });
+  
+
 module.exports = router;
